d962393fd6c85cd8cdf9b4c03ab63d0b1b5468e5
Oumaima Stitini <oumaima.stitini@exo-it.com>
2018-11-08 13:25:05 +0000

    master

---
+++ src/com/software/shop/user/view/UserCtrl.java
@@ -0,0 +1,23 @@
+package com.software.shop.user.view;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class UserCtrl  extends Application {
+	private Stage primaryStage;
+	private static BorderPane mainLayout;
+
+	@Override
+	public void start(Stage primaryStage) throws IOException {
+	
+	}
+
+	
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
